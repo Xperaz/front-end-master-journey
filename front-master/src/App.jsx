8aa3d0b4768d43dd89f96d443f64229d9d4fa7bb
Azedine ouhadou <aouhadou@student.1337.ma>
2023-05-17 17:31:11 +0100

    start learning journey with front end master

---
+++ front-master/src/App.jsx
@@ -0,0 +1,18 @@
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+import "./style.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>Adopt Me!</h1>
+      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Popper" animal="cat" breed="Cockatiel" />
+      <Pet name="Rrank" animal="bird" breed="Mixed" /> */}
+
+      <SearchParams />
+    </>
+  );
+};
+
+export default App;
---
+++ front-master/src/SearchParams.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const breeds = [];
+
+  return (
+    <div className="search-params">
+      <form action="">
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+                setBreed(e.target.value)
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
